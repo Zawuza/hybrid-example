fc39feed7a02dd3ae46cc806c21a097bc9c009db
ZAwuza <andreizawuza@gmail.com>
2017-11-10 23:52:03 +0100

    Fail

---
+++ fMain.pas
@@ -0,0 +1,33 @@
+unit fMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.WebBrowser,
+  FMX.Controls.Presentation, FMX.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    WebBrowser1: TWebBrowser;
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  WebBrowser1.URL := ExtractFilePath(ParamStr(0)) + 'app.html';
+  WebBrowser1.Navigate;
+end;
+
+end.

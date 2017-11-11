203e5db40b788a6d8b1cf653eeb52df1991e65ea
ZAwuza <andreizawuza@gmail.com>
2017-11-11 12:01:47 +0100

    Final commit

--- fMain.pas
+++ fMain.pas
@@ -5,16 +5,19 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.WebBrowser,
-  FMX.Controls.Presentation, FMX.StdCtrls;
+  FMX.Controls.Presentation, FMX.StdCtrls, System.IOUtils, SG.ScriptGate;
 
 type
   TForm1 = class(TForm)
     WebBrowser1: TWebBrowser;
+    OpenDialog1: TOpenDialog;
+    SaveDialog1: TSaveDialog;
     procedure FormCreate(Sender: TObject);
   private
-    { Private declarations }
+    FGate: TScriptGate;
   public
-    { Public declarations }
+    procedure OpenFile;
+    procedure SaveFile(Content: string);
   end;
 
 var
@@ -27,7 +30,22 @@ implementation
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   WebBrowser1.URL := ExtractFilePath(ParamStr(0)) + 'app.html';
+  FGate := TScriptGate.Create(Self,WebBrowser1,'delphi');
   WebBrowser1.Navigate;
+end;
+
+procedure TForm1.OpenFile;
+var Content: string;
+begin
+ if OpenDialog1.Execute then
+    Content := TFile.ReadAllText(OpenDialog1.FileName);
+ FGate.CallScript('document.getElementById("txtx").innerText = "' + Content + '";' ,nil);
+end;
+
+procedure TForm1.SaveFile(Content: string);
+begin
+ if SaveDialog1.Execute then
+   TFile.WriteAllText(SaveDialog1.FileName,Content);
 end;
 
 end.
--- hybrid.dpr
+++ hybrid.dpr
@@ -3,7 +3,16 @@ program hybrid;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  fMain in 'fMain.pas' {Form1};
+  fMain in 'fMain.pas' {Form1},
+  SG.ScriptGate in 'dependencies\scriptgate\SG.ScriptGate.pas',
+  SG.ScriptGate.Win in 'dependencies\scriptgate\SG.ScriptGate.Win.pas',
+  SG.ScriptGateLog in 'dependencies\scriptgate\SG.ScriptGateLog.pas',
+  SG.WebBrowserHelper in 'dependencies\scriptgate\SG.WebBrowserHelper.pas',
+  Androidapi.JNI.SGWebClient in 'dependencies\scriptgate\Androidapi.JNI.SGWebClient.pas',
+  SG.ScriptGate.Android in 'dependencies\scriptgate\SG.ScriptGate.Android.pas',
+  SG.ScriptGate.Android.SGWebClientBridge in 'dependencies\scriptgate\SG.ScriptGate.Android.SGWebClientBridge.pas',
+  SG.ScriptGate.iOS in 'dependencies\scriptgate\SG.ScriptGate.iOS.pas',
+  SG.ScriptGate.Mac in 'dependencies\scriptgate\SG.ScriptGate.Mac.pas';
 
 {$R *.res}
 
